50a60bb428e1e23da0551bf5da328d0b5881ebb2
Rob_The_Deer <86634577+robthedeer@users.noreply.github.com>
2023-12-20 21:53:52 +0300

    Initial Commit

---
+++ Practise_C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practise_C++
+//
+//  Created by Sofia on 20.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
